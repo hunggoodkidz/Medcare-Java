b407b059f905f1bb2ed699025871f95c93ce86d2
hunggoodkidz <ninbook0708@gmail.com>
2023-03-27 19:38:07 +0700

    update database from export

---
+++ hospital_2.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 27, 2023 at 02:13 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hospital_2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fullname` varchar(45) NOT NULL,
+  `gender` varchar(45) NOT NULL,
+  `age` varchar(45) NOT NULL,
+  `appoint_date` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phno` varchar(45) NOT NULL,
+  `diseases` varchar(45) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `status` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `appointment`
+--
+
+INSERT INTO `appointment` (`id`, `user_id`, `fullname`, `gender`, `age`, `appoint_date`, `email`, `phno`, `diseases`, `doctor_id`, `address`, `status`) VALUES
+(1, 2, 'Tran Chan Hung', 'male', '18', '2023-04-06', 'ninbook0708@gmail.com', '0979810953', 'ho Lao', 1, '1321456', 'ok');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(45) NOT NULL,
+  `birthday` varchar(45) NOT NULL,
+  `qualification` varchar(45) NOT NULL,
+  `specialist` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `phone` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`id`, `full_name`, `birthday`, `qualification`, `specialist`, `email`, `phone`, `password`) VALUES
+(1, 'Dr Hung', '2023-03-06', 'Bang MBBS', 'Dentist', 'ninbook0708@gmail.com', '0979810953', '123456');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialist`
+--
+
+CREATE TABLE `specialist` (
+  `id` int(11) NOT NULL,
+  `spec_name` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `specialist`
+--
+
+INSERT INTO `specialist` (`id`, `spec_name`) VALUES
+(1, 'Dentist');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_dtls`
+--
+
+CREATE TABLE `user_dtls` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_dtls`
+--
+
+INSERT INTO `user_dtls` (`id`, `full_name`, `email`, `password`) VALUES
+(1, 'Trần Chấn Hưng', 'ninbook0708@gmail.com', '123456'),
+(2, 'Ai Do', 'test1@gmail.com', '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `uid` (`user_id`),
+  ADD KEY `did` (`doctor_id`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `specialist`
+--
+ALTER TABLE `specialist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_dtls`
+--
+ALTER TABLE `user_dtls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `specialist`
+--
+ALTER TABLE `specialist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_dtls`
+--
+ALTER TABLE `user_dtls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `did` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `uid` FOREIGN KEY (`user_id`) REFERENCES `user_dtls` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
